ac922ee72682f09f3ff9a349fc9ae81c7ead583b
Mactavish2520 <131664354+Mayuresh-G-Mhatre@users.noreply.github.com>
2025-03-13 13:03:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ function createTask() {
     if (taskInput.value.length == 0) {
         alert("The task field is blank. Enter a task name and try again.");
     } else {
+        // SVG icon for delete button (Trash Icon)
+        const trashIconSVG = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <polyline points="3 6 5 6 21 6"></polyline>
+                <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"></path>
+                <path d="M10 11L10 17"></path>
+                <path d="M14 11L14 17"></path>
+                <path d="M4 6L20 6"></path>
+                <path d="M9 6L9 4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"></path>
+            </svg>
+        `;
+
         // This block inserts HTML that creates each task into the task area div element
         taskSection.innerHTML += 
         `<div class="task">
@@ -27,7 +39,7 @@ function createTask() {
                 <p>${taskInput.value}</p>
             </label>
             <div class="delete">
-                <ion-icon name="trash-outline"></ion-icon>
+                ${trashIconSVG}  <!-- SVG icon inserted here -->
             </div>
         </div>`;
 
